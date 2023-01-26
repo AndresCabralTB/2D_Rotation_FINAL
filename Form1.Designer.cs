c5400d8cc934e6566266be4f7be51cf511495e18
AndresCabralTB <122487634+AndresCabralTB@users.noreply.github.com>
2023-01-25 23:18:37 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace _2D_Rotation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PCT_CANVAS = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PCT_CANVAS
+            // 
+            this.PCT_CANVAS.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PCT_CANVAS.Location = new System.Drawing.Point(12, 12);
+            this.PCT_CANVAS.MaximumSize = new System.Drawing.Size(900, 900);
+            this.PCT_CANVAS.Name = "PCT_CANVAS";
+            this.PCT_CANVAS.Size = new System.Drawing.Size(648, 291);
+            this.PCT_CANVAS.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PCT_CANVAS.TabIndex = 0;
+            this.PCT_CANVAS.TabStop = false;
+            this.PCT_CANVAS.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(134, 368);
+            this.textBox1.MaximumSize = new System.Drawing.Size(116, 24);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(116, 24);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "00";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 313);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "1 - Pivot in Window Origin";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Silver;
+            this.button2.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(134, 313);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(116, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "2 - Pivot in Box Center ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Silver;
+            this.button3.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(256, 313);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(116, 40);
+            this.button3.TabIndex = 2;
+            this.button3.Tag = "";
+            this.button3.Text = "3 - Box Center in Origin";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 368);
+            this.label1.MaximumSize = new System.Drawing.Size(300, 300);
+            this.label1.MinimumSize = new System.Drawing.Size(116, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ENTER ANGLE:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(672, 404);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.PCT_CANVAS);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PCT_CANVAS)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PCT_CANVAS;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace _2D_Rotation
+{
+    public partial class Form1 : Form
+    {
+        static Bitmap bmp;
+        float x, y;
+        float textBB,angle;
+        static Graphics g;
+        private PointF a, b, c, d;
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+            bmp = new Bitmap(PCT_CANVAS.Width, PCT_CANVAS.Height);
+            PCT_CANVAS.Image = bmp;
+            g = Graphics.FromImage(bmp);
+
+            x = PCT_CANVAS.Width / 2;
+            y = PCT_CANVAS.Height / 2;
+
+            //g.TranslateTransform(x, y); //Change origin from (0,0) to (x,y) //CHANGE LATER ALL THE COORDINATES
+
+            //Coordinations and code for cross in the middle
+             
+            a = new PointF(0,y);
+            b = new PointF(x * 2, y);
+            c = new PointF(x,0);
+            d = new PointF(x, y * 2);
+
+            g.DrawLine(Pens.Red, a, b); //Horizontal Line
+            g.DrawLine(Pens.Red, c, d); //Vertical Line
+
+            //CODE FOR THE BOX IN THE MIDDLE
+
+            PointF aa = new PointF(x, y);
+            PointF bb = new PointF(x, y -100);
+            PointF cc = new PointF(x + 100, y-100);
+            PointF dd = new PointF(x + 100, y);
+
+            g.DrawLine(Pens.Black, aa, bb);
+            g.DrawLine(Pens.Black, bb, cc);
+            g.DrawLine(Pens.Black, cc, dd);
+            g.DrawLine(Pens.Black, dd, aa);
+
+            PCT_CANVAS.Invalidate();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ResetPCT(); //Function to restart canvas 
+            angleText();
+
+            PointF p1, p2, p3, p4;
+            p1 = new PointF(0, 0);
+            p2 = new PointF(0, 100);
+            p3 = new PointF(100, 100);
+            p4 = new PointF(100, 0);
+
+            Render(p1, p2);
+            Render(p2, p3);
+            Render(p3, p4);
+            Render(p4, p1);
+
+
+            PCT_CANVAS.Invalidate();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+           
+
+            ResetPCT();
+            angleText();
+
+            a = new PointF(0,0);
+            b = new PointF(0, 100);
+            c = new PointF(100, 100);
+            d = new PointF(100, 0); 
+
+            RenderLine(a, b);
+            RenderLine(b, c);
+            RenderLine(c, d);
+            RenderLine(d, a);
+            PCT_CANVAS.Invalidate();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ResetPCT();
+            angleText();
+
+            PointF p1, p2, p3, p4;
+            p1 = new PointF(-50,-50);
+            p2 = new PointF(50, -50);
+            p3 = new PointF(50, 50);
+            p4 = new PointF(-50, 50);
+
+            Render(p1, p2);
+            Render(p2, p3);
+            Render(p3, p4);
+            Render(p4, p1);
+            PCT_CANVAS.Invalidate();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+  
+
+        private void Render(PointF aa, PointF bb)
+        {
+            PointF a2, b2;
+            int Sx = bmp.Width / 2;
+            int Sy = bmp.Height / 2;
+
+            a2 = new PointF(Sx + aa.X, Sy - aa.Y);
+            b2 = new PointF(Sx + bb.X, Sy - bb.Y);
+
+            a2.X = Sx + (float)((aa.X * Math.Cos(angle)) - (aa.Y * Math.Sin(angle)));
+            a2.Y = Sy - (float)((aa.X * Math.Sin(angle)) + (aa.Y * Math.Cos(angle)));
+            b2.X = Sx + (float)((bb.X * Math.Cos(angle)) - (bb.Y * Math.Sin(angle)));
+            b2.Y = Sy - (float)((bb.X * Math.Sin(angle)) + (bb.Y * Math.Cos(angle)));
+
+            g.DrawLine(Pens.Black, a2, b2);
+        }
+
+        private PointF Rotate(PointF a)
+        {
+            PointF b = new PointF();
+            b.X = (float)((a.X * Math.Cos(angle)) - (a.Y * Math.Sin(angle)));
+            b.Y = (float)((a.X * Math.Sin(angle)) + (a.Y * Math.Cos(angle))); 
+            return b;
+        }
+
+        private PointF TranslateToCenter(PointF a)
+        {
+            int Sx = (bmp.Width / 2);  // origen central en x
+            int Sy = (bmp.Height / 2); // origen central en y
+
+            return new PointF(Sx + a.X, Sy - a.Y);
+        }
+
+        private PointF Translate(PointF a, PointF b)
+        {
+            return new PointF(a.X + b.X, a.Y + b.Y);
+        }
+
+        private void RenderLine(PointF a, PointF b)
+        {
+            
+            a = Translate(a, new PointF(-50, -50));
+            b = Translate(b, new PointF(-50, -50));
+
+            PointF c = Rotate(a);
+            PointF d = Rotate(b);
+
+            c = TranslateToCenter(c);
+            d = TranslateToCenter(d);
+
+            c = Translate(c, new PointF(50, -50));
+            d = Translate(d, new PointF(50, -50));
+
+            g.DrawLine(Pens.Black, c, d);
+
+        }
+        private void ResetPCT()
+        {
+            g.Clear(Color.White);
+
+            g.DrawLine(Pens.Red, 0, y, x *2, y); //Horizontal Line
+            g.DrawLine(Pens.Red, x,0,x,y*2); //Vertical Line
+        }
+
+        public void angleText()
+        {
+            textBB = float.Parse(textBox1.Text);
+            angle = (float)(Math.PI * textBB) / 180;
+        }
+        
+    }
+}
